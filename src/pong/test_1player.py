490ef4113145839c58fa561d0a4ca604d69e51eb
prialgon <alpri.gonzalez@gmail.com>
2026-01-03 14:28:00 +0100

    added text and automatic recalibration

--- src/pong/test_1player.py
+++ src/pong/test_1player.py
@@ -4,6 +4,7 @@ from ball import Ball
 from constants import *
 from tracker import Tracker
 from fps import FPS
+from text_manager import add_text
 
 cap = cv2.VideoCapture(0)
 
@@ -23,6 +24,8 @@ trained = False
 kcf_right = None
 kcf_left = None
 
+recalibrate_timer = 5
+
 time.time()
 while True:
     start_time = time.time()
@@ -39,7 +42,7 @@ while True:
 
     if key == ord('q') or key == 27:
         break
-    elif key == ord('s'):
+    elif recalibrate_timer < 0 and recalibrate_timer != -100:
         kcf_right = Tracker(frame, 900, 1280, 0, 720, "right")
         trained = True
 
@@ -50,11 +53,24 @@ while True:
         
         kcf_right.update(frame, 900, 1280, 0, 720)
 
-        if kcf_right.detected:
-            frame = kcf_right.draw_box(frame)
-            ball.update_paddle_collisions(kcf_right.roi)
+        # if kcf_right.detected:
+            
+        frame = kcf_right.draw_box(frame)
+        ball.update_paddle_collisions(kcf_right.roi)
+
+        if not kcf_right.detected:
+            recalibrate_timer = 5 if recalibrate_timer < 0 else recalibrate_timer
+            frame = add_text(
+                frame, f"Recalibrating in {int(recalibrate_timer)}...", "top")
+            frame = cv2.rectangle(frame, (1000, 200), (1280, 480),
+                                  color=(0, 0, 255), thickness=2)
+        else:
+            recalibrate_timer = -100
+
 
     else:
+        frame = add_text(frame, f"Place your hand in the red square", "top")
+        frame = add_text(frame, f"{int(recalibrate_timer)}", "center", 5)
         frame = cv2.rectangle(frame, (1000, 200), (1280, 480),
                               color=(0, 0, 255), thickness=2)
 
@@ -66,8 +82,11 @@ while True:
 
     deltatime = end_time - start_time
 
-    frame = fps.update(frame, deltatime)
+    if recalibrate_timer > 0:
+        recalibrate_timer -= deltatime
 
+    frame = fps.update(frame, deltatime)
+    
     cv2.imshow(window_name, frame)
 
 cap.release()
---
+++ src/pong/text_manager.py
@@ -0,0 +1,18 @@
+import cv2
+
+def add_text(frame: cv2.typing.MatLike, text: str, position: str, size: float = 1):
+    
+    text_width, text_height = cv2.getTextSize(
+        text, cv2.FONT_HERSHEY_SIMPLEX, size, 2)[0]
+
+    if position == "top":
+        TopCoordinates = (int(frame.shape[1] / 2) - int(text_width / 2),
+                             50 + int(text_height / 2))
+        cv2.putText(frame, text, TopCoordinates, cv2.FONT_HERSHEY_SIMPLEX,
+                size, (0, 0, 0), 2, cv2.LINE_AA)
+    elif position == "center":
+        CenterCoordinates = (int(frame.shape[1] / 2) - int(text_width / 2),
+                             int(frame.shape[0] / 2) + int(text_height / 2))
+        cv2.putText(frame, text, CenterCoordinates, cv2.FONT_HERSHEY_SIMPLEX,
+                    size, (0, 0, 0), 2, cv2.LINE_AA)
+    return frame
